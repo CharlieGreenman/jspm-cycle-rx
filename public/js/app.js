3d2a6e485cf3cd6f26897a542b98dfe150bef6bc
Charlie Greenman <charie@razroo.com>
2016-06-28 22:38:13 -0400

    Change body background color. Fix file structure for js files.

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,6 @@ import _ from 'lodash';
 const date = moment().format();
 
 /*eslint-disable */
-import './scss/main.scss!';
+import '../scss/main.scss!';
 
 _.forEach(date, char => console.log(char));
